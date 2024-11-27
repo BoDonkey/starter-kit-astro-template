e9f18412ca10e32a2aaea0e27cffbe101d02a1b3
Alex Gilbert <alex@apostrophecms.com>
2024-11-27 15:21:23 -0500

    remove assets module and clean up app.js config

--- app.js
+++ app.js
@@ -7,32 +7,23 @@ export default apostrophe({
     // Apostrophe module configuration
     // *******************************
     //
-    // NOTE: most configuration occurs in the respective modules' directories.
+    // Most configuration occurs in the respective modules' directories.
     // See modules/@apostrophecms/page/index.js for an example.
     //
     // Any modules that are not present by default in Apostrophe must at least
     // have a minimal configuration here to turn them on: `moduleName: {}`
-    // ***********************************************************************
-    // `className` options set custom CSS classes for Apostrophe core widgets.
-    '@apostrophecms/rich-text-widget': {
-    },
-    '@apostrophecms/image-widget': {
-    },
-    '@apostrophecms/video-widget': {
-    },
-    // `asset` supports the project's webpack build for client-side assets.
-    asset: {},
-    // '@apostrophecms/vite': {},
-    // '@apostrophecms/asset': {
-    //   options: {
-    //     hmr: false
-    //   }
-    // },
-    // The project's first custom page type.
-    'default-page': {},
+    //
+    // ********************************
+
+    // pieces
     article: {},
-    'article-page': {},
     author: {},
+
+    // pages
+    'default-page': {},
+    'article-page': {},
+
+    // widgets
     'grid-layout-widget': {},
     'accordion-widget': {},
     'card-widget': {},
