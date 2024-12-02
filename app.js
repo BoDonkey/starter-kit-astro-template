fba75d0d69ff8754b5ea1a27c1219dc90f0c57d8
Robert Means <robert@robertmeans.net>
2024-12-02 12:04:16 -0500

    Decide on Vite

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ export default apostrophe({
     // have a minimal configuration here to turn them on: `moduleName: {}`
 
     // *********************************
-    // Add Vite build
-    '@apostrophecms/vite': {},
+    // Add Vite build - uncomment to enable
+    // '@apostrophecms/vite': {},
 
     // `className` options set custom CSS classes for Apostrophe core widgets.
     '@apostrophecms/rich-text-widget': {},
